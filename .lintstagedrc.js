683a836b2e74515ba0fe223513ff2791cea7dec8
hassaku_0616 <ayumuogawa0616@gmail.com>
2025-11-20 19:33:15 +0900

    コミット時に単体テストを実行する

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -6,5 +6,5 @@
 module.exports = {
   "**/*.{js,ts,jsx,tsx,html,css,json,jsonc}":
     "bunx biome check --write --unsafe",
-  "**/*.{js,ts,jsx,tsx}": () => "bunx tsc --noEmit",
+  "**/*.{js,ts,jsx,tsx}": () => ["bunx tsc --noEmit", "bun run test"],
 };
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
+import { resolve } from "node:path";
 import dotenv from "dotenv";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     env: dotenv.config({ path: ".env.development" }).parsed,
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
   },
 });
