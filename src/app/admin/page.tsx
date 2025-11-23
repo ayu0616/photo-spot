ee896140f5480629764f6cde06752d62cb0dbd1f
hassaku_0616 <ayumuogawa0616@gmail.com>
2025-11-23 15:20:58 +0900

    feat: implement basic admin page UI with a link to trips management

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,12 @@
-import { notFound } from "next/navigation";
+import Link from "next/link";
 
 export default function Page() {
-  return notFound();
+  return (
+    <div className="p-6 container mx-auto">
+      <h1 className="text-2xl font-bold mb-4">Admin</h1>
+      <p>
+        <Link href={"/admin/trips"}>旅行</Link>
+      </p>
+    </div>
+  );
 }
