8041586a52e437029b9a962bbe2227b3b3f83bf4
hassaku_0616 <ayumuogawa0616@gmail.com>
2026-01-05 19:00:37 +0900

    refactor: Move QueryClient instantiation into TanstackQueryProvider.

--- src/provider/tanstack-query-provider.tsx
+++ src/provider/tanstack-query-provider.tsx
@@ -1,14 +1,21 @@
 "use client";
 
-import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { ReactNode } from "react";
-import { queryClient } from "@/lib/query-client";
 
 export const TanstackQueryProvider = ({
   children,
 }: {
   children: ReactNode;
 }) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false, // Window focus refetching is often not needed for simple data display
+        staleTime: 1000 * 60 * 5, // 5 minutes
+      },
+    },
+  });
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
